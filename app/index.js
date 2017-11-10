aae1dd33945571af1ddff48cadb494262e96462e
shelvadim <shelgachev@yahoo.com>
2017-11-09 21:12:49 -0800

    speakers

--- app/index.js
+++ app/index.js
@@ -1,12 +1,44 @@
-var http = require('http');
-var myServer = http.createServer(function(request, response){
-  response.writeHead(200,{"Content-Type":"text/html"});
-  response.write('<h1>My site</h1>');
-  response.end();
+var express = require('express');
+var app = express();
+var dataFile = require('./data/data.json');
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function (req, res) {
+
+  res.send(`
+    <h1>Hello</h1>
+    <p>Meetups</p>
+    `);
 });
 
 
+app.get('/speakers', function (req, res) {
+  var info = '';
+  dataFile.speakers.forEach(function(item){
+    info+=`
+      <li>
+        <h2>${item.name}</h2>
+        <p>${item.summary}</p>
+      </li>
+    `;
+  });
+  res.send(`
+    <h1>Speakers</h1>
+    ${info}
+    `);
+});
 
-myServer.listen(3000);
+app.get('/speakers/:speakerid', function (req, res) {
+  var speaker = dataFile.speakers[req.params.speakerid];
+  res.send(`
+    <h1>${speaker.title}</h1>
+    <h2>with ${speaker.name}</h2>
+    <p>${speaker.summary}</p>
+    `);
+});
 
-console.log('running on port 3000!');
+
+var server = app.listen(app.get('port'), function () {
+  console.log('listening on port 3000!'+app.get('port'));
+});
