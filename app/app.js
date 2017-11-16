b6d95aef3bbe3d7935baa1f2c0b3c5dff9403198
shelvadim <shelgachev@yahoo.com>
2017-11-15 20:48:57 -0800

    ejs

--- app/app.js
+++ app/app.js
@@ -5,6 +5,10 @@ var dataFile = require('./data/data.json');
 
 app.set('port', process.env.PORT || 3000);
 app.set('appData', dataFile);
+app.set('view engine', 'ejs');
+app.set('views','app/views');
+
+app.locals.siteTitle='Roux meetup';
 
 app.use(express.static('app/public'));
 app.use(require('./routes/index'));
@@ -17,4 +21,4 @@ var server = app.listen(app.get('port'), function () {
   console.log('listening on port 3000!'+app.get('port'));
 });
 
-reload(server, app);
+reload(app);
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,26 @@ var router=express.Router();
 
 
 router.get('/', function (req, res) {
+  var data = req.app.get('appData');
+  var pagePhotos=[];
 
-  res.send(`
+data.speakers.forEach(function(item){
+  pagePhotos = pagePhotos.concat(item.artwork);
+});
+
+  res.render('index',{
+    pageTitle:'Home',
+    artwork: pagePhotos,
+    pageID:'home'
+  });
+
+  /*res.send(`
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <h1>Welcome to meetup</h1>
     <img src="/images/misc/background.jpg" alt="background" style="height:300px;">
     <p>Meetups</p>
     <script src="/reload/reload.js"></script>
-    `);
+    `);*/
 });
 
 module.exports=router;
